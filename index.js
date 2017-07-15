7a975cd56d25317095900832edd1f74e1afd2c47
jsmeyers <jsmeyers@users.noreply.github.com>
2017-07-15 10:51:41 -0400

    messing around to convert from denon to sharp

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Service, Characteristic;
-var Denon = require('./lib/denon');
+var Denon = require('./lib/aquos');
 var inherits = require('util').inherits;
 var pollingtoevent = require('polling-to-event');
 
@@ -8,27 +8,30 @@ module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
 
-    homebridge.registerAccessory('homebridge-denon-marantz-avr', 'DenonMarantzAVR', DenonAVRAccessory);
+    homebridge.registerAccessory('homebridge-sharp-tv', 'SharpTV', SharpTVAccessory);
 };
 
-function DenonAVRAccessory(log, config) {
+function SharpTVAccessory(log, config) {
     this.log = log;
 	var that = this;
 	
     this.config = config;
-    this.ip = config['ip'];
-    this.name = config['name'];
+    this.ip = config['ip'] || '10.19.27.220';
+    this.name = config['name'] || 'test';
+    this.port = config['port'] || 10002;
+    this.username = config['username'] || 'a';
+    this.password = config['password'] || 'a';
 
     this.defaultInput = config['defaultInput'] || null;
     this.defaultVolume = config['defaultVolume'] || null;
     this.minVolume = config['minVolume'] || 0;
-    this.maxVolume = config['maxVolume'] || 70;
+    this.maxVolume = config['maxVolume'] || 60;
 	this.doPolling = config['doPolling'] || false;
 	
 	this.pollingInterval = config['pollingInterval'] || "60";
 	this.pollingInterval = parseInt(this.pollingInterval)
 
-    this.denon = new Denon(this.ip);
+    this.aquos = new Aquos(this.ip, this.port, this.username, this.password);
 	
 	this.setAttempt = 0;
 	this.state = false;
@@ -59,12 +62,12 @@ function DenonAVRAccessory(log, config) {
 }
 
 
-DenonAVRAccessory.prototype.getPowerState = function (callback, context) {
+SharpTVAccessory.prototype.getPowerState = function (callback, context) {
 	
 	if ((!context || context != "statuspoll") && this.doPolling) {
 		callback(null, this.state);
 	} else {
-	    this.denon.getPowerState(function (err, state) {
+	    this.aquos.power(function (err, state) {
 	        if (err) {
 	            this.log(err);
 	            callback(null, false);
@@ -76,8 +79,7 @@ DenonAVRAccessory.prototype.getPowerState = function (callback, context) {
 	}
 };
 
-
-DenonAVRAccessory.prototype.setPowerState = function (powerState, callback, context) {
+SharpTVAccessory.prototype.setPowerState = function (powerState, callback, context) {
 	var that = this;
 
 	//if context is statuspoll, then we need to ensure that we do not set the actual value
@@ -88,24 +90,24 @@ DenonAVRAccessory.prototype.setPowerState = function (powerState, callback, cont
 	
 	this.setAttempt = this.setAttempt+1;
 	
-    this.denon.setPowerState(powerState, function (err, state) {
+    this.aquos.power(powerState, function (err, state) {
         if (err) {
             this.log(err);
         } else {
             if(powerState && this.defaultInput) {
-                this.denon.setInput(this.defaultInput, function (error) {
+                this.aquos.setInput(this.defaultInput, function (error) {
                     if (error) {
                         this.log('Error setting default input. Please check your config');
                     }
                 }.bind(this));
             }
-            this.log('denon avr powered %s', state);
+            this.log('Sharp TV powered %s', state);
         }
     }.bind(this));
 
     if (powerState && this.defaultVolume) {
         setTimeout(function () {
-            this.denon.setVolume(this.defaultVolume, function (err) {
+            this.aquos.volume(this.defaultVolume, function (err) {
                 if (err) {
                     this.log('Error setting default volume');
                 }
@@ -117,8 +119,8 @@ DenonAVRAccessory.prototype.setPowerState = function (powerState, callback, cont
     callback(null);
 };
 
-DenonAVRAccessory.prototype.getVolume = function (callback) {
-    this.denon.getVolume(function (err, volume) {
+SharpTVAccessory.prototype.getVolume = function (callback) {
+    this.aquos.volume(function (err, volume) {
         if (err) {
             this.log('get Volume error: ' + err)
             callback(err);
@@ -130,9 +132,9 @@ DenonAVRAccessory.prototype.getVolume = function (callback) {
     }.bind(this))
 };
 
-DenonAVRAccessory.prototype.setVolume = function (pVol, callback) {
+SharpTVAccessory.prototype.setVolume = function (pVol, callback) {
     var volume = Math.round(pVol / 100 * this.maxVolume);
-    this.denon.setVolume(volume, function (err) {
+    this.aquos.volume(volume, function (err) {
         if (err) {
             this.log('set Volume error: ' + err);
         } else {
@@ -142,8 +144,8 @@ DenonAVRAccessory.prototype.setVolume = function (pVol, callback) {
     }.bind(this))
 };
 
-DenonAVRAccessory.prototype.setMuteState = function (state, callback) {
-    this.denon.setMuteState(state, function (err) {
+SharpTVAccessory.prototype.setMuteState = function (state, callback) {
+    this.aquos.mute(state, function (err) {
         if (err) {
             this.log('set mute error: ' + err);
             callback(err);
@@ -153,8 +155,8 @@ DenonAVRAccessory.prototype.setMuteState = function (state, callback) {
     }.bind(this));
 };
 
-DenonAVRAccessory.prototype.getMuteState = function (callback) {
-    this.denon.getMuteState(function (err, state) {
+SharpTVAccessory.prototype.getMuteState = function (callback) {
+    this.aquos.mute(function (err, state) {
         if (err) {
             this.log('get mute error: ' + err);
             callback(err);
@@ -164,12 +166,12 @@ DenonAVRAccessory.prototype.getMuteState = function (callback) {
     }.bind(this))
 };
 
-DenonAVRAccessory.prototype.getServices = function () {
+SharpTVAccessory.prototype.getServices = function () {
     var informationService = new Service.AccessoryInformation();
 
     informationService
         .setCharacteristic(Characteristic.Name, this.name)
-        .setCharacteristic(Characteristic.Manufacturer, this.type || 'Denon');
+        .setCharacteristic(Characteristic.Manufacturer, this.type || 'Sharp');
 
     this.switchService = new Service.Switch(this.name);
     this.switchService.getCharacteristic(Characteristic.On)
